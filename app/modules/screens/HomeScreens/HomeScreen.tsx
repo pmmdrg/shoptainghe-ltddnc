e8bd724d954b43039a11bd69e0d653106c196ef9
vuquangpham <vuquangpham2909@gmail.com>
2024-03-19 10:09:31 +0700

    fix: wrong file path

--- app/modules/screens/HomeScreens/HomeScreen.tsx
+++ app/modules/screens/HomeScreens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   View,
   Text,
@@ -7,16 +7,16 @@ import {
   SafeAreaView,
   Image,
   TextInput,
-} from 'react-native';
+} from "react-native";
 
 import {
   backgroundColor,
   borderColor,
   textColors,
-} from '../../../assets/colors';
+} from "../../../assets/colors";
 
-import HomeSection from '../../components/HomeSection';
-import NavigationItem from '../../components/NavigationItem';
+import HomeSection from "../../components/HomeSection";
+import NavigationItem from "../../components/NavigationItem";
 
 enum sectionType {
   category,
@@ -29,35 +29,35 @@ enum sectionType {
 const data = [
   {
     id: 1,
-    name: 'Headphone',
+    name: "Headphone",
     price: 500000,
     rate: 4.5,
     rateCount: 80,
   },
   {
     id: 2,
-    name: 'Headphone',
+    name: "Headphone",
     price: 500000,
     rate: 4.5,
     rateCount: 80,
   },
   {
     id: 3,
-    name: 'Headphone',
+    name: "Headphone",
     price: 500000,
     rate: 4.5,
     rateCount: 80,
   },
   {
     id: 4,
-    name: 'Headphone',
+    name: "Headphone",
     price: 500000,
     rate: 4.5,
     rateCount: 80,
   },
   {
     id: 5,
-    name: 'Headphone',
+    name: "Headphone",
     price: 500000,
     rate: 4.5,
     rateCount: 80,
@@ -65,10 +65,10 @@ const data = [
 ];
 
 const HomeScreen = ({ navigation }) => {
-  const [productName, setProductName] = useState('');
+  const [productName, setProductName] = useState("");
 
   const handleLogin = () => {
-    navigation.navigate('Login');
+    navigation.navigate("Login");
   };
 
   return (
@@ -76,64 +76,64 @@ const HomeScreen = ({ navigation }) => {
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Shop Tai nghe</Text>
         <Image
-          source={require('../../assets/icons/notification.png')}
+          source={require("../../../assets/icons/notification.png")}
           style={styles.notiIcon}
         />
         <Image
-          source={require('../../assets/icons/cart.png')}
+          source={require("../../../assets/icons/cart.png")}
           style={styles.cartIcon}
         />
       </View>
       <ScrollView>
         <View style={styles.searchContainer}>
           <TextInput
-            placeholder='Tìm kiếm tên sản phẩm'
+            placeholder="Tìm kiếm tên sản phẩm"
             value={productName}
             onChangeText={(text) => setProductName(text)}
             style={styles.input}
           />
-          <Image source={require('../../assets/icons/search.png')} />
+          <Image source={require("../../../assets/icons/search.png")} />
         </View>
         <HomeSection
           type={sectionType.featured}
-          title='Sản phẩm nổi bật'
+          title="Sản phẩm nổi bật"
           list={data}
         />
         <HomeSection
           type={sectionType.bestSeller}
-          title='Bán chạy nhất'
+          title="Bán chạy nhất"
           list={data}
         />
         <HomeSection
           type={sectionType.newArrival}
-          title='Hàng mới về'
+          title="Hàng mới về"
           list={data}
         />
         <HomeSection
           type={sectionType.discount}
-          title='Ưu đãi đặc biệt'
+          title="Ưu đãi đặc biệt"
           list={data}
         />
       </ScrollView>
       <View style={styles.navigation}>
         <NavigationItem
-          iconSrc={require('../../assets/icons/home.png')}
-          title='TRANG CHỦ'
+          iconSrc={require("../../../assets/icons/home.png")}
+          title="TRANG CHỦ"
           onPress={() => {}}
         />
         <NavigationItem
-          iconSrc={require('../../assets/icons/heart.png')}
-          title='YÊU THÍCH'
+          iconSrc={require("../../../assets/icons/heart.png")}
+          title="YÊU THÍCH"
           onPress={() => {}}
         />
         <NavigationItem
-          iconSrc={require('../../assets/icons/bag.png')}
-          title='ĐƠN HÀNG'
+          iconSrc={require("../../../assets/icons/bag.png")}
+          title="ĐƠN HÀNG"
           onPress={() => {}}
         />
         <NavigationItem
-          iconSrc={require('../../assets/icons/profile.png')}
-          title='ĐĂNG NHẬP'
+          iconSrc={require("../../../assets/icons/profile.png")}
+          title="ĐĂNG NHẬP"
           onPress={handleLogin}
         />
       </View>
@@ -153,30 +153,30 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginVertical: 30,
     marginHorizontal: 25,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    position: 'relative',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "relative",
   },
   header: {
     height: 55,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderBottomColor: borderColor.primaryBorder,
     borderBottomWidth: 1,
-    position: 'relative',
+    position: "relative",
   },
   headerTitle: {
     color: textColors.blueText,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 18,
   },
   notiIcon: {
-    position: 'absolute',
+    position: "absolute",
     right: 70,
   },
   cartIcon: {
-    position: 'absolute',
+    position: "absolute",
     right: 30,
   },
   input: {
@@ -186,8 +186,8 @@ const styles = StyleSheet.create({
   },
   navigation: {
     height: 60,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     borderTopColor: borderColor.primaryBorder,
     borderTopWidth: 1,
   },
